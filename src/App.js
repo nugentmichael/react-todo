7c9b788cadc34793e75425bbb4d828a4c3f508d2
nugentmichael <nugent.michael@gmail.com>
2019-05-17 11:17:16 -0400

    Add constructor along with handleInput and addItem event functions.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoList from './TodoList.js'
 import './App.css';
 
-function App() {
-  return (
-	<div className="App">
-		<TodoList />
-	</div>
-  );
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			items: [],
+			currentItem: { text: '', key: '' },
+		}
+	}
+
+	handleInput = e => {
+		console.log('Hello input');
+	}
+
+	addItem = () => {
+		console.log('Hello add item');
+	}
+
+	render() {
+		return (
+			<div className="App">
+		    	<TodoList addItem={this.addItem} />
+			</div>
+		)
+	}
 }
 
 export default App;
